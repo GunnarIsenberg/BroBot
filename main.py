32d5919295cb6d2ef1bc98effc469cc4f074e85e
GunnarIsenberg <52142814+GunnarIsenberg@users.noreply.github.com>
2021-12-13 19:10:51 +0000

    bot responds!

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import discord
+import requests
+import json
+
+client = discord.Client()
+
+def get_quote():
+	response = requests.get("https://zenquotes.io/api/random")
+	json_data = json.loads(response.text)
+	quote = json_data[0]['q'] + " -" + json_data[0]['a']
+	return(quote)
+
+@client.event
+async def on_ready():
+	print('We have logged in as {0.user}'.format(client) )
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	
+	if message.content.startswith('$hello'):
+		await message.channel.send("Hello!");
+
+	if message.content.startswith('$inspire'):
+		curQuote = get_quote()
+		await message.channel.send(curQuote)
+
+client.run(os.environ['Token'])
